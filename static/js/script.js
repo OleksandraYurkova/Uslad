669e9766a629b4b9ab7d8e3d40d70830e294e00d
OleksandraYurkova <oleksandra.o.yurkova@ukd.edu.ua>
2025-10-25 19:58:09 +0300

    first commit

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+// // // main.js - автоматичне підсвічування активного пункту
+// const navLinks = document.querySelectorAll('.nav .hed_text');
+// const path = window.location.pathname;
+
+// navLinks.forEach(link => {
+//     if (link.getAttribute('href') === path) {
+//         link.classList.add('active');
+//     }
+// });
