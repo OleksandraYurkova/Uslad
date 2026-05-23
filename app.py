7cd629c71a2e49630011dc2d9497e58e52edf227
OleksandraYurkova <oleksandra.o.yurkova@ukd.edu.ua>
2026-05-23 17:24:47 +0300

    fix procfile

--- app.py
+++ app.py
@@ -364,4 +364,9 @@ def contacts():
     return render_template("contacts.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
